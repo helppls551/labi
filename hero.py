ce2869ea4a72e7a58ab8b402c7536ed04f461a89
helppls551 <136239458+helppls551@users.noreply.github.com>
2023-10-15 15:22:31 +0300

    Add files via upload

--- hero.py
+++ hero.py
@@ -43,4 +43,82 @@ class Hero():
         else:
             self.cameraBind()
 
-    def turn_right
+    def turn_left(self):
+        self.hero.setH((self.hero.getH() + 5)%360)
+
+    def turn_right(self):
+        self.hero.setH((self.hero.getH() - 5)% 360)
+    
+    def look_at(self,angle):
+        '''возвращает координаты, в которые переместится персонаж, если он делает шаг в направлении angle(
+            три скобы отображаются в информации об функции)'''
+        x_from = round(self.hero.getX())
+        y_from = round(self.hero.getY())
+        z_from = round(self.hero.getZ())
+
+        dx, dy = self.check_dir(angle)
+        x_to = x_from +dx
+        y_to = y_from + dy
+        return x_to,y_to,z_from
+    
+    def just_move(self,angle):
+        '''перемещается в нужные коодинаты в любом случае'''
+        pos = self.look_at(angle)
+        self.hero.setPos(pos)
+    
+    def move_to(self,angle):
+        if self.mode:
+            self.just_move(angle)
+    
+    def check_dir(self,angle):
+        if angle >= 0 and angle <= 20:
+            return(0,-1)
+        elif angle<= 65:
+            return(1,-1)
+        elif angle<= 110:
+            return(1,1)
+        elif angle()<=155:
+            return(1,1)
+        elif angle()<= 200:
+            return(0,1)
+        elif angle()<= 245:
+            return(-1,1)
+        elif angle()<=290:
+            return(-1,1)
+        elif angle()<= 335:
+            return(-1,-1)
+        else:
+            return(0,-1)
+    
+    def forward(self):
+        angle = (self.hero.getH())%360
+        self.move_to(angle)
+
+    def back(self):
+        angle = (self.hero.getH()+ 180) %360
+        self.move_to(angle)
+    
+    def left(self):
+        angle = (self.hero.getH() +90)% 360
+        self.move_to(angle)
+    def right(self):
+        angle = (self.hero.getH() -90) %360
+        self.move_to(angle)
+    
+    def accept_events(self):
+        base.accept(key_turn_left,self.turn_left)
+        base.accept(key_turn_left + 'repeat',self.turn_left)
+        base.accept(key_turn_right,self.turn_right)
+        base.accept(key_turn_right + 'repeat',self.turn_right)
+        base.accept(key_forward,self.forward)
+        base.accept(key_forward + 'repeat',self.forward)
+        base.accept(key_back,self.back)
+        base.accept(key_back+ 'repeat',self.back)
+        base.accept(key_left,self.left)
+        base.accept(key_left+ 'repeat',self.left)
+        base.accept(key_right,self.right)
+        base.accept(key_right+ 'repeat',self.right)
+        base.accept(key_switch_camera,self.changeView)
+        
+        
+
