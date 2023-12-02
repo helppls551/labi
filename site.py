c64b962be4ebcb192b7e3a9ff20e0c719c97020a
helppls551 <136239458+helppls551@users.noreply.github.com>
2023-12-02 13:46:26 +0300

    Add files via upload

--- site.py
+++ site.py
@@ -1,11 +1,13 @@
 from flask import Flask,render_template,url_for
-
-app= Flask(__name__)
+import os
+folder = os.getcwd
+app = Flask(__name__,template_folder= 'F:\GR\templates' ,static_folder= '')
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    names = []
+    return render_template('base.html',lst = names)
 
 
 @app.route('/about')
